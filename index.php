557991857a3887740414c74e38ab0680924e6d38
GitTuanKiet <tuank1792@gmail.com>
2023-12-13 15:06:21 +0700

    kiet

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$controller = $_GET['controller'] ?? 'article';
+$action = $_GET['action'] ?? 'index';
+
+if ($controller === 'auth') {
+    $controllerClass = 'AuthController';
+} else {
+    $controllerClass = 'ArticleController';
+}
+
+require_once "controllers/$controllerClass.php";
+$controllerInstance = new $controllerClass();
+$controllerInstance->$action();
