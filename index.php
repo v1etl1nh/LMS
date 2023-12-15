8422d42512239d279d67a2de91b1c3fb2b6a9fe7
Nguyễn Trung Kiên <vumihao@gmail.com>
2023-12-15 14:16:00 +0700

    kien

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Get the controller and action from the URL
+$controller = isset($_GET['controller']) ? $_GET['controller'] : 'auth';
+$action = isset($_GET['action']) ? $_GET['action'] : 'login';
+
+// Create the controller class name
+$controllerClass = ucfirst($controller) . 'Controller';
+
+// Instantiate the controller
+$controllerFile = "lms/controllers/$controllerClass.php";
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+        $controllerInstance = new $controllerClass();
+        $controllerInstance->$action();
+} else {
+    echo "Controller not found.".$controllerFile;
+}
+?>
