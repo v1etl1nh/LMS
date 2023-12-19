6b140731c47f29aa2fb28236d595b6972f21bbfe
GitTuanKiet <tuank1792@gmail.com>
2023-12-19 23:47:34 +0700

    git add . git commit -m "login" git push origin kiet

--- lms/views/auth/login.php
+++ lms/views/auth/login.php
@@ -107,7 +107,7 @@ height: 100%;
         </div>
         <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
             <div class="text-white mb-3 mb-md-0">
-                Copyright © 2023. Kiệt ngu
+                Copyright © 2023. Kiệt
             </div>
             <div>
                 <a href="#!" class="text-white me-4">
